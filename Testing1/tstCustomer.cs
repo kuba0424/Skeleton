b814e4749509740df90b379ca0ffa463708c4778
p2716670 <p2716670@GWHXX680WIN016W>
2024-05-02 10:08:57 +0100

    test for customers

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
