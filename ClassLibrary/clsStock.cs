d378992adf25bfceef3911645d6b3d05ce094091
Euan Christie <P2716291@my365.dmu.ac.uk>
2024-05-07 14:13:15 +0100

    added properties

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,23 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool itemAvailable { get; set; }
+
+        public DateTime itemDate { get; set; }
+
+        public int itemID { get; set; }
+
+        public int itemStock { get; set; }
+
+        public double itemSize { get; set; }
+
+        public double itemPrice { get; set; }
+
+        public string itemDescription { get; set; }
+
+
     }
 }
--- Testing4/tstStock.cs
+++ Testing4/tstStock.cs
@@ -15,5 +15,88 @@ namespace Testing4
             //test to see that it exists
             Assert.IsNotNull(Stock);
         }
+
+        
+        
+        [TestMethod]
+
+        public void AvailablePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            Stock.itemAvailable = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.itemAvailable, TestData);
+
+        }
+
+        [TestMethod]
+
+        public void DateAddedPropertyOK()
+        {
+            clsStock Stock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            Stock.itemDate = TestData;
+            Assert.AreEqual(Stock.itemDate, TestData);
+        }
+
+        [TestMethod]
+
+        public void ItemIDOK()
+        {
+            clsStock Stock = new clsStock();
+            Int32 TestData = 1;
+            Stock.itemID = TestData;
+            Assert.AreEqual(Stock.itemID, TestData);
+        }
+
+        [TestMethod]
+
+        public void ItemStockOK()
+        {
+            clsStock Stock = new clsStock();
+            Int32 TestData = 1;
+            Stock.itemStock = TestData;
+            Assert.AreEqual(Stock.itemStock, TestData);
+
+        }
+
+        [TestMethod]
+
+        public void itemSizeOK()
+        {
+            clsStock Stock = new clsStock();
+            double TestData = 1.5;
+            Stock.itemSize = TestData;
+            Assert.AreEqual(Stock.itemSize, TestData);
+        }
+
+
+        [TestMethod]
+
+        public void itemPriceOK()
+        {
+            clsStock Stock = new clsStock();
+            double TestData = 1.5;
+            Stock.itemPrice = TestData;
+            Assert.AreEqual(Stock.itemPrice, TestData);
+        }
+
+
+        [TestMethod]
+
+        public void itemDescriptionOK()
+        {
+            clsStock Stock = new clsStock();
+            string TestData = "This is an item of clothing";
+            Stock.itemDescription = TestData;
+            Assert.AreEqual(Stock.itemDescription, TestData);
+        }
+
+
     }
+
 }
