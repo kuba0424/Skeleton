7db7b0f54c81d2ec876e09626d3d264834497d3f
p2713508 <p2713508@GWHXX683WIN011W>
2024-05-07 13:29:33 +0100

    Added bunch of test for my table properties

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool IsAdmin { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int StaffId { get; set; }
+        public string StaffNickName { get; set; }
+        public string StaffUser { get; set; }
+        public string StaffPass { get; set; }
     }
 }
