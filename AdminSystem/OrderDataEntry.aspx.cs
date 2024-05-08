8440c78a597d4460dd4e105ae2b2440a4f40c426
P2715003 <P2715003@GWHXX680WIN017W>
2024-05-08 14:14:36 +0100

    added a simple presentation layer.

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the customer address
+        AnOrder.CustomerAddress = txtCustomerAddress.Text;
+        AnOrder.TotalPrice = Convert.ToDouble(txtTotalPrice.Text);
+        AnOrder.OrderDispatched = chkDispatched.Checked;
+        AnOrder.PaymentInformation = Convert.ToInt32(txtPaymentInformation.Text);
+        AnOrder.DispatchDate = Convert.ToDateTime(DateTime.Now);
+        //Store the address in the session object
+        Session["AnOrder"] = AnOrder;
+        //navigate to the view page
+        Response.Redirect("OrderViewer.aspx");
     }
 }
--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //get the data from the session object
+        AnOrder = (clsOrder)Session["AnOrder"];
+        //display the customer address for this entry
+        Response.Write(AnOrder.CustomerAddress);
+        Response.Write(AnOrder.TotalPrice);
+        Response.Write(AnOrder.OrderDispatched);
+        Response.Write(AnOrder.PaymentInformation);
+        Response.Write(AnOrder.DispatchDate);
     }
 }
