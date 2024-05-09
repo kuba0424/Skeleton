1d54a0f112d11a619806c53d886b9c81a9cb239d
p2716670 <p2716670@GWHXX680WIN016W>
2024-05-09 10:18:48 +0100

    today and yesterday

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new ClassLibrary.clsCustomer();
+        //get the data from the session object
+        AnCustomer = (clsCustomer)Session["Ancustomer"];
+        //display the usernnamer for the entry
+        Response.Write(AnCustomer.Username);
 
     }
 }
