3425242d76dbc35987f01eb0aba6669f26f49d10
Euan Christie <P2716291@my365.dmu.ac.uk>
2024-05-20 19:54:16 +0100

    euan prac uh 12

--- AdminSystem/StockList.aspx
+++ AdminSystem/StockList.aspx
@@ -5,11 +5,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            z-index: 1;
+            left: 10px;
+            top: 34px;
+            position: absolute;
+            height: 409px;
+            width: 460px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstStockList" runat="server" CssClass="auto-style1"></asp:ListBox>
     </form>
 </body>
 </html>
--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page has been displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStock();
+        }
+    }
 
+    public void DisplayStock()
+    {
+        //create an instgance of the stock collection
+        clsStockCollection Stock = new clsStockCollection();
+        //set the data source to the lsit opf stock int he collection
+        lstStockList.DataSource = Stock.StockList;
+        //set the name of the primary key
+        lstStockList.DataValueField = "Id";
+        //set the data field to display
+        lstStockList.DataTextField = "ItemId";
+        //bind the data to the list
+        lstStockList.DataBind();
     }
 }
