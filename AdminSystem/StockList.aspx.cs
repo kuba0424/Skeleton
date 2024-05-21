7875e08c423bcecd0a2a9f59f4a641212fc07cdc
Euan Christie <P2716291@my365.dmu.ac.uk>
2024-05-21 12:32:16 +0100

    prac 13a

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -31,4 +31,12 @@ public partial class _1_List : System.Web.UI.Page
         //bind the data to the list
         lstStockList.DataBind();
     }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        //store -1 into the session object to indicate this is a new record
+        Session["Id"] = -1;
+        //redirect to the data entry page
+        Response.Redirect("StockDataEntry.aspx");
+    }
 }
