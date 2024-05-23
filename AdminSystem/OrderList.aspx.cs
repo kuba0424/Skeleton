5df8b59f42cebea00311bcaf41cc90f162493adc
P2715003 <P2715003@GWHXX680WIN017W>
2024-05-23 09:34:04 +0100

    prac 12 changes

--- AdminSystem/OrderList.aspx.cs
+++ AdminSystem/OrderList.aspx.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayOrders();
+        }
+    }
 
+    void DisplayOrders()
+    {
+        //create an instance of the order collection
+        clsOrderCollection Orders = new clsOrderCollection();
+        //set the data source to list of orders in the collection
+        lstOrderList.DataSource = Orders.OrderList;
+        //set the name of the primary key
+        lstOrderList.DataValueField = "OrderId";
+        //set the data field to display
+        lstOrderList.DataTextField = "CustomerAddress";
+        //bind data to the list
+        lstOrderList.DataBind();
     }
 }
